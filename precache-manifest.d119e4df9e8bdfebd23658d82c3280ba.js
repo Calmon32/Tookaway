da89f5644e1c9f6377493eb099de7fc945197c2c
Lucas Calmon <lucas.calmon11@gmail.com>
2019-09-11 15:18:17 +0200

    Updates

---
+++ precache-manifest.d119e4df9e8bdfebd23658d82c3280ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f220fa744471d69adf140d78f8a6a1d",
+    "url": "/Tookaway/index.html"
+  },
+  {
+    "revision": "921db13bfeda5f5db53a",
+    "url": "/Tookaway/static/css/main.c998a1ef.chunk.css"
+  },
+  {
+    "revision": "06aa82b06df07712721b",
+    "url": "/Tookaway/static/js/2.aadc7fde.chunk.js"
+  },
+  {
+    "revision": "921db13bfeda5f5db53a",
+    "url": "/Tookaway/static/js/main.8679882f.chunk.js"
+  },
+  {
+    "revision": "2de672708dfe87f1e7c8",
+    "url": "/Tookaway/static/js/runtime~main.3f97099b.js"
+  }
+]);
