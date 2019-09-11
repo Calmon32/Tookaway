56aeedc648b100955101eb70a64991d124115cf7
Lucas Calmon <lucas.calmon11@gmail.com>
2019-09-11 13:01:16 +0200

    Git forgot one file...

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,23 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
 
 var apiRouter = require('./routes/api');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false, // true = .sass and false = .scss
-  sourceMap: true
-}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// 
 app.use('/api', apiRouter);
 
 app.get('*', (req,res) =>{
   res.sendFile(path.join(__dirname, 'public/index.html'));
-});
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
 });
 
 // catch 404 and forward to error handler
